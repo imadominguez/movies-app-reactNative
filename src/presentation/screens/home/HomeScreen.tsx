7f2c251c391279caac880eaa68af03239d2ee8eb
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-03-02 19:05:38 -0300

    scrollview de las mejores peliculas

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {useMovies} from '../../hooks/useMovies';
+import {ScrollView} from 'react-native-gesture-handler';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {PosterCarousel} from '../../components/movies/PosterCarousel';
 
 export const HomeScreen = () => {
   const {isLoading, nowPlaying, popular, topRated, upcoming} = useMovies();
 
+  const {top} = useSafeAreaInsets();
+
+  if (isLoading) {
+    return <Text>Cargando...</Text>;
+  }
+
   return (
-    <View
-      style={{
-        padding: 20,
-      }}>
-      <Text>HomeScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={{marginTop: top + 20, paddingBottom: 30}}>
+        <PosterCarousel movies={nowPlaying} />
+      </View>
+    </ScrollView>
   );
 };
