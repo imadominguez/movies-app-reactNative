7202d1746ec28dbaf460ee3c51c44091ecfc48f4
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-03-03 11:53:46 -0300

    add dotenv

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,10 +1,11 @@
+import {THE_MOVIE_KEY} from '@env';
 import {AxiosAdapter} from './http/axios.adapter';
 
 export const movieDBFetcher = (params?: Record<string, unknown>) =>
   new AxiosAdapter({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     params: {
-      api_key: process.env.THE_MOVIE_KEY ?? 'aaa8b4f61fad42db740ac2136e8cafd6',
+      api_key: THE_MOVIE_KEY ?? '',
       lenguage: 'es',
       ...params,
     },
