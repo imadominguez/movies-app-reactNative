864625969cd596e7761ae040ff8edd44f0b8bb03
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-03-03 11:20:28 -0300

    fix: custom hook movie

--- src/presentation/hooks/useMovie.ts
+++ src/presentation/hooks/useMovie.ts
@@ -2,30 +2,46 @@ import {useEffect, useMemo, useState} from 'react';
 import * as UsesCases from '../../core/use-cases';
 import {movieDBFetcher} from '../../config/adapters/movieDB.adapter';
 import {FullMovie} from '../../core/entities/movie.entity';
+import {Cast} from '../../core/entities/cast.entity';
+import {AxiosAdapter} from '../../config/adapters/http/axios.adapter';
 
 export const useMovie = (movieId: number) => {
   const [isLoading, setIsLoading] = useState<Boolean>(true);
   const [movie, setMovie] = useState<FullMovie>();
+  const [cast, setCast] = useState<Cast[]>();
   // Este custom hook realizara la peticion para buscar la movie por id a la db
 
   useEffect(() => {
-    loadMovie.then(m => {
-      setMovie(m);
+    loadMovie.then(({movie, cast}) => {
+      setMovie(movie);
+      setCast(cast);
       setIsLoading(false);
     });
   }, [movieId]);
 
   const loadMovie = useMemo(async () => {
-    const moviePromise = await UsesCases.getMovieByIdUseCase(
+    // TODO: manejar errores
+    const moviePromise = UsesCases.getMovieByIdUseCase(
       movieDBFetcher(),
       movieId,
     );
 
-    return moviePromise;
+    const castPromise = UsesCases.getMovieCastUseCase(
+      movieDBFetcher(),
+      movieId,
+    );
+
+    const [movie, cast] = await Promise.all([moviePromise, castPromise]);
+
+    return {
+      movie,
+      cast,
+    };
   }, [movie]);
 
   return {
     isLoading,
     movie,
+    cast,
   };
 };
